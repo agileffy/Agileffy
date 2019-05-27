efea56e4b63f6e0fc9a47eecf1573d44b2c82763
ichn-hu <zfhu16@fudan.edu.cn>
2019-05-27 10:34:31 +0800

    remove useless vue files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Agileffy, your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Agileffy, your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 })
 export default class Home extends Vue {}
