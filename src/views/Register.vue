5df4ec44af0fadbe13f7bafb31d1c0a6ec1e78f4
ichn-hu <zfhu16@fudan.edu.cn>
2019-06-07 21:11:59 +0800

    fix tslint error for axios.js

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -124,6 +124,7 @@ export default {
                 );
             },
             email: (value) => {
+                // tslint:disable-next-line
                 const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 return pattern.test(value) || 'Invalid e-mail.';
             },
